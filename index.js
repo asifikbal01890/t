3b02f168e6beccf78e61a0af564c103b2baf4885
asifikbal01890 <asifikbal01890@gmail.com>
2025-05-01 17:37:15 +0600

    cokies veryfi

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // Create an Express application
 const app = express();
-const port = process.env.PORT || 4000; 
+const port = process.env.PORT || 4000;
 
 // Middleware setup
 app.use(cors(
@@ -15,10 +15,25 @@ app.use(cors(
         origin: "http://localhost:5173", // Replace with your client URL
         credentials: true, // Allow credentials (cookies) to be sent
     }
-)); 
-app.use(express.json()); 
-app.use(cookieParser()); // Parse cookies from incoming requests
- // Log incoming requests to the console
+));
+app.use(express.json());
+app.use(cookieParser());
+const logger = (req, res, next) => {
+
+    const token = req.cookies.token; // Get the token from the cookie
+
+    if (!token) return res.status(401).send("Access denied. No token provided.");
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).send("Invalid token.");
+        req.user = user; // Attach the user to the request object
+    });
+    
+    
+    next();
+};
+
+
 
 // MongoDB connection URI using credentials from environment variables
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rjpks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -41,15 +56,16 @@ async function run() {
         const db = client.db("jobApply");
         const JobsCollection = db.collection("allJobs");
         const ApplicationsCollection = db.collection("applications");
-        
+
+
         app.post("/jwt", (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
-            res.cookie("token", token, { 
-            httpOnly: true, 
-            secure: false,         // not required for localhost
-            sameSite: 'strict',
-            maxAge: 3600000
+            res.cookie("token", token, {
+                httpOnly: true,
+                secure: false,         // not required for localhost
+                sameSite: 'strict',
+                maxAge: 3600000
             }); // Set the cookie with the token
             res.send({ message: "Token generated and cookie set" });
         });
@@ -110,9 +126,18 @@ async function run() {
             const result = await ApplicationsCollection.find().toArray();
             res.send(result);
         });
+
+        app.get("/applications/:email", logger, async (req, res) => {
+            const email = req.params.email;
+            
+            const query = { userEmail: email }
+            const result = await ApplicationsCollection.find(query).toArray();
+            res.send(result);
+        });
+
         app.get("/applications/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {job_id: id}
+            const query = { job_id: id }
             const result = await ApplicationsCollection.find(query).toArray();
             res.send(result);
         });
@@ -120,7 +145,7 @@ async function run() {
             const id = req.params.id;
             const status = req.body;
             const filter = { _id: new ObjectId(id) };
-            const updateDoc = { $set: {status: status.status} }
+            const updateDoc = { $set: { status: status.status } }
             const result = await ApplicationsCollection.updateOne(filter, updateDoc);
             res.send(result);
         });
